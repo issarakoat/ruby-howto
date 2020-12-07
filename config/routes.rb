bd3df55a2bf8a65bec0d071443466f174ebc43e7
issarakoat <weeravut.issarakoat@gmail.com>
2020-12-07 14:20:38 +0000

    remove home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :howtodos
-  get 'home/index'
-  root 'home#index'
+  root 'howtodos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
