c3da2e8d5de9f67a84269c3cda79226affc10194
issarakoat <weeravut.issarakoat@gmail.com>
2020-12-10 14:59:14 +0000

    enable to edit role

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home/activity'
   devise_for :users
   resources :howtodos
-  resources :users, only: [:index]
+  resources :users, only: [:index, :edit, :show, :update]
   root 'howtodos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
