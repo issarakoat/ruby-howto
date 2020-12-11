e8408b9be6e5860aea2a609e3091e079cd1e6ac9
issarakoat <weeravut.issarakoat@gmail.com>
2020-12-11 06:40:39 +0000

    add comment inside howtodo

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,10 @@ class Comment < ApplicationRecord
   belongs_to :howtodo
   validates :content, presence: true
   has_rich_text :content
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+
+  def to_s
+    content
+  end
 end
