d18204c43044da4652c22504035839517280f0a7
issarakoat <weeravut.issarakoat@gmail.com>
2020-12-13 16:17:14 +0000

    .

--- app/helpers/howtodos_helper.rb
+++ app/helpers/howtodos_helper.rb
@@ -1,2 +1,15 @@
 module HowtodosHelper
+  def rate_button(howtodo)
+    link_to 'Add rating scope', new_howtodo_rating_path(howtodo_id: @howtodo.id), class: 'btn btn-primary'
+    user_howtodo = howtodo.ratings.where(user: current_user)
+    if current_user
+      if user_howtodo.any?
+        if user_howtodo.pending_rating.any?
+            link_to 'Add rating scope after if thing', new_howtodo_rating_path(howtodo_id: @howtodo.id), class: 'btn btn-primary'
+        else
+            link_to 'Thanks for reviewing! Your Review', howtodo_path(howtodo)
+        end
+      end
+    end
+  end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,5 @@
 class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :howtodo
+  scope :pending_rating, -> { where(rate: [0, nil, ""]) }
 end
