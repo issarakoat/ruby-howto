0af2c066265d436059865a507ef5f74d98ea94b7
issarakoat <weeravut.issarakoat@gmail.com>
2020-12-14 05:57:09 +0000

    add trakable to rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,6 +2,9 @@ class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :howtodo
     
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+    
   after_save do
     unless rate.nil? || rate.zero?
       howtodo.update_rating
