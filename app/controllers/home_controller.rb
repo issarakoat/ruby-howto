8541c70f5d6b3ab7df6ea7d825e51137313a6509
issarakoat <weeravut.issarakoat@gmail.com>
2020-12-15 05:23:15 +0000

    add more dynamic on the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   def index
     @howtodos = Howtodo.all.limit(3)
     @latest_howtodos = Howtodo.all.limit(3).order(created_at: :desc)
+    
+    @top_rated_howtodos = Howtodo.order(average_rating: :desc, created_at: :desc).limit(3)
+    @popular_howtodos = Howtodo.order(comments_count: :desc, created_at: :desc).limit(3)
   end
   def activity
     @activities = PublicActivity::Activity.all
